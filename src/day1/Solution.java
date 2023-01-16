98be4394694f7cd2a75d36d8ba60a89e6fe2e34f
Xander <contact@xander479.co.uk>
2023-01-16 23:38:37 +0000

    Task 1

--- src/day1/Solution.java
+++ src/day1/Solution.java
@@ -1,5 +1,36 @@
 package day1;
 
-public class Solution {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class Solution {
+  public static void main(String[] args) {
+    task1();
+  }
+  public static void task1() {
+    try(BufferedReader reader = new BufferedReader(new FileReader("src/day1/input.txt"))) {
+      int mostCalories = 0;
+      int currentElf = 0;
+      String food = reader.readLine();
+
+      while(food != null) {
+        if(food.equals("")) {
+          if(currentElf > mostCalories) {
+            mostCalories = currentElf;
+          }
+          currentElf = 0;
+        }
+        else {
+          currentElf += Integer.parseInt(food);
+        }
+        food = reader.readLine();
+      }
+
+      System.out.println(mostCalories);
+    }
+    catch(IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
