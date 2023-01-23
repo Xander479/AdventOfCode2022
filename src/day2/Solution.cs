620d2a5576df87fd268ba5ddf401a5714570d279
Xander <contact@xander479.co.uk>
2023-01-23 20:55:33 +0000

    Set up day 2

---
+++ src/day2/Solution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace day2 {
+  class Solution {
+    public static void Main(string[] args) {
+      task1();
+      task2();
+    }
+
+    public static void task1() { }
+
+    public static void task2() { }
+  }
+}
