cf1769f78c5a181f9b619593949e972d1b6f2e3f
Xander <contact@xander479.co.uk>
2023-01-24 00:41:10 +0000

    Task 1

---
+++ src/day2/Choice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace day2 {
+  public enum Choice {
+    ROCK = 1,
+    PAPER,
+    SCISSORS
+  }
+  public static class Extensions {
+    private static readonly int values = Enum.GetNames(typeof(Choice)).Length;
+    public static int versus(this Choice choice, Choice opponent) {
+      // 1 if this wins, 2 if opponent wins, 0 if tie
+      return (values + (int)choice - (int)opponent) % values;
+    }
+  }
+}
--- src/day2/Solution.cs
+++ src/day2/Solution.cs
@@ -4,13 +4,46 @@ using System.Linq;
 using System.Text;
 
 namespace day2 {
-  class Solution {
+  public class Solution {
     public static void Main(string[] args) {
       task1();
       task2();
     }
 
-    public static void task1() { }
+    public static void task1() {
+      using StreamReader reader = File.OpenText("input.txt"); // May have to move input.txt to whichever directory you're running from
+      int score = 0;
+      string? line = String.Empty;
+      while((line = reader.ReadLine()) != null) {
+        Choice opponent = line.Split(' ')[0] switch {
+          "A" => Choice.ROCK,
+          "B" => Choice.PAPER,
+          "C" => Choice.SCISSORS,
+          _ => throw new Exception("Unexpected input")
+        };
+        Choice me = line.Split(' ')[1] switch {
+          "X" => Choice.ROCK,
+          "Y" => Choice.PAPER,
+          "Z" => Choice.SCISSORS,
+          _ => throw new Exception("Unexpected input")
+        };
+        switch(me.versus(opponent)) {
+          case 0: // draw
+            score += (int)me + 3;
+            break;
+          case 1: // win
+            score += (int)me + 6;
+            break;
+          case 2: // loss
+            score += (int)me;
+            break;
+          default:
+            throw new Exception("value = " + me.versus(opponent));
+        }
+      }
+
+      Console.WriteLine(score);
+    }
 
     public static void task2() { }
   }
